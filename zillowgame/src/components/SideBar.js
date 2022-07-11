862f8302132a3451e07bb4f20e52a161604095a6
featheru <71615880+featheru@users.noreply.github.com>
2022-07-11 19:48:57 -0400

    Version 0.1.0 Release

--- zillowgame/src/components/SideBar.js
+++ zillowgame/src/components/SideBar.js
@@ -7,7 +7,7 @@ export default props => {
     <Menu>
       <h2 id = "menu-item">Beavers Try to Beat the Zestimate!</h2>
       <img src = {beaver} width ="100" height = "100" alt ="Beaver Pic--TBD"></img>
-      <a className="menu-item">Steps to Using Beat the Zestimate!</a>
+      <a className="menu-item">Steps to Beating the Zestimate!</a>
       <ol className="menu-item">
         <li>Login Using Accounts Page.  DB Admin must add you to the system.</li>
         <li>Use DisplayProperties to view all your properties</li>
